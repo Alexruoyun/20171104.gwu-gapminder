361e9362d089e5e6bd5b15a34227cbb8e95c2a6a
Wuyang Qian <bknks.qwy@gmail.com>
2017-11-04 11:28:10 -0400

    Revert "add comments"
    This reverts commit 1ccde3fe3f96e977f682b133fa48e121e55cff27.

--- script.R
+++ script.R
@@ -1,7 +1,5 @@
-##load tidy packages
 library(tidyverse)
 
-##download the files from the website
 download.file("http://fmichonneau.github.io/2017-11-03-gwu/gapminder.csv","data/gapminder.csv")
 
 gapminder <- read.csv("data/gapminder.csv")
