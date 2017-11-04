efd0111d94b49161287ae84ee4870f322c3ce380
Wuyang Qian <bknks.qwy@gmail.com>
2017-11-04 10:37:59 -0400

    add life expectancy for Canada

--- script.R
+++ script.R
@@ -5,7 +5,7 @@ download.file("http://fmichonneau.github.io/2017-11-03-gwu/gapminder.csv","data/
 gapminder <- read.csv("data/gapminder.csv")
 
 ##Challenge:
-## calculate the average life expectancy by continent
+##Calculate the average life expectancy by continent
 ##(and save it in a variable called life_exp_continent)
 
 life_exp_continent <- gapminder %>% 
@@ -13,3 +13,10 @@ life_exp_continent <- gapminder %>%
   summarise(
     mean_life_exp = mean(lifeExp)
   )
+
+##Challenge
+##get the life expectancy by year for Canada
+
+life_exp_canada <- gapminder %>% 
+  filter(country == "Canada") %>% 
+  select(year,lifeExp)
